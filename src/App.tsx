9940bf7623b3c7fb5f8a0dc1771e130102cd6a93
DDR23 <roboreserva.andre@gmail.com>
2024-08-28 20:54:08 -0300

    remove:{removendo pastas de rotas inexistentes}

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import '@mantine/core/styles.css';
 import '@mantine/notifications/styles.css';
-import { MantineProvider } from '@mantine/core';
 import theme from '../theme'
+import { MantineProvider } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
-import { Outlet } from 'react-router-dom';
 
 export default function App() {
   return (
     <MantineProvider theme={theme} defaultColorScheme='auto'>
       <Notifications />
-      <Outlet />
     </MantineProvider>
   );
 }
