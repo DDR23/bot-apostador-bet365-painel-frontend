dfe21ece97c8ba2dac83c3a1db72626c58329e66
DDR23 <roboreserva.andre@gmail.com>
2024-08-29 23:50:18 -0300

    fix:{padronizando nome do arquivo}

--- src/utils/GetSocket.ts
+++ src/utils/GetSocket.ts
@@ -2,10 +2,10 @@ import { io, Socket } from "socket.io-client";
 
 let socket: Socket | null = null;
 
-const getSocket = () => {
+const GetSocket = () => {
   if (!socket) socket = io('http://localhost:8080');
   if (!socket) throw new Error('Falha ao criar a conexão com socket');
   return socket;
 };
 
-export default getSocket;
+export default GetSocket;
