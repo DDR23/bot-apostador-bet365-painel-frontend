7ea2f9fdb5b058f3bc6c351380a7851d5e29675c
DDR23 <roboreserva.andre@gmail.com>
2024-08-31 23:46:49 -0300

    feat:{adicionando type de config}

---
+++ src/types/TypeConfigTenis.ts
@@ -0,0 +1,19 @@
+export interface EstrategiesTenis {
+  _id?: number;
+  DIFF_SET: number | null;
+  DIFF_POINT: number | null;
+  MULTIP: number;
+  ODD_VALUE: number;
+}
+
+export interface TypeConfigTenis {
+  _id?: number;
+  USER: string;
+  PASSWORD: string;
+  TIME_START: string;
+  TIME_FINISH: string;
+  STOP_WIN?: number | null;
+  STOP_LOSS?: number | null;
+  ESTRATEGIES?: EstrategiesTenis[];
+  STATUS?: boolean;
+}
