9097740aba7c7a175c69b1ea6d618a54bfe8f179
DDR23 <roboreserva.andre@gmail.com>
2024-09-05 20:43:37 -0300

    fix:{ajustando schema de strategy}

--- src/schemas/SchemaStrategyTenis.ts
+++ src/schemas/SchemaStrategyTenis.ts
@@ -1,11 +1,11 @@
 import * as yup from "yup";
 
 export const SchemaStrategyTenisCreate = yup.object().shape({
-  STRATEGY_DIFF_SET_TYPE: yup.string().optional(),
+  STRATEGY_DIFF_SET_TYPE: yup.mixed<'diff' | 'exato'>().oneOf(['diff', 'exato']).optional(),
   STRATEGY_DIFF_SET: yup.number().optional(),
   STRATEGY_DIFF_SET_PLAYER1: yup.number().optional(),
   STRATEGY_DIFF_SET_PLAYER2: yup.number().optional(),
-  STRATEGY_DIFF_POINT_TYPE: yup.string().optional(),
+  STRATEGY_DIFF_POINT_TYPE: yup.mixed<'diff' | 'exato'>().oneOf(['diff', 'exato']).optional(),
   STRATEGY_DIFF_POINT: yup.number().optional(),
   STRATEGY_DIFF_POINT_PLAYER1: yup.number().optional(),
   STRATEGY_DIFF_POINT_PLAYER2: yup.number().optional(),
@@ -17,11 +17,11 @@ export const SchemaStrategyTenisCreate = yup.object().shape({
 });
 
 export const SchemaStrategyTenisUpdate = yup.object().shape({
-  STRATEGY_DIFF_SET_TYPE: yup.string().optional(),
+  STRATEGY_DIFF_SET_TYPE: yup.mixed<'diff' | 'exato'>().oneOf(['diff', 'exato']).optional(),
   STRATEGY_DIFF_SET: yup.number().optional(),
   STRATEGY_DIFF_SET_PLAYER1: yup.number().optional(),
   STRATEGY_DIFF_SET_PLAYER2: yup.number().optional(),
-  STRATEGY_DIFF_POINT_TYPE: yup.string().optional(),
+  STRATEGY_DIFF_POINT_TYPE: yup.mixed<'diff' | 'exato'>().oneOf(['diff', 'exato']).optional(),
   STRATEGY_DIFF_POINT: yup.number().optional(),
   STRATEGY_DIFF_POINT_PLAYER1: yup.number().optional(),
   STRATEGY_DIFF_POINT_PLAYER2: yup.number().optional(),
