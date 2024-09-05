75727d7b4955f794ed75e5ff012f399a80440409
DDR23 <roboreserva.andre@gmail.com>
2024-09-04 21:47:14 -0300

    fix:{agora ProviderInitBot dispara notificações corretamente}

--- src/utils/ProviderInitBot.tsx
+++ src/utils/ProviderInitBot.tsx
@@ -1,7 +1,18 @@
 import { TypeConfig } from "../types/TypeConfig";
 import GetSocket from "./GetSocket";
+import ProviderNotification from "./ProviderNotification";
 
+//TODO - essa função deve emitir e ouvir evento de BOT não de CONFIG
 export default function ProviderInitBot(config: TypeConfig) {
   const socket = GetSocket();
-  socket.emit('CONFIG_PUT', config._id, { CONFIG_STATUS: !config.CONFIG_STATUS })
+  const handleConfigPutRes = (response: { title: string, message: string }) => {
+    const { title } = response;
+    const message = 'bot iniciado'
+    ProviderNotification({ title, message });
+
+    socket.off('CONFIG_PUT_RES', handleConfigPutRes);
+  };
+  socket.on('CONFIG_PUT_RES', handleConfigPutRes);
+
+  socket.emit('CONFIG_PUT', config._id, { CONFIG_STATUS: !config.CONFIG_STATUS });
 }
