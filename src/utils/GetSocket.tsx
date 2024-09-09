d06e8a11e111c5e665b4dd7f746718ac9c031fd1
omegatech-andre <omega.ti_ind@outlook.com>
2024-09-09 17:22:50 -0300

    save

--- src/utils/GetSocket.tsx
+++ src/utils/GetSocket.tsx
@@ -3,7 +3,7 @@ import { io, Socket } from "socket.io-client";
 let socket: Socket | null = null;
 
 const GetSocket = () => {
-  if (!socket) socket = io('http://localhost:8080');
+  if (!socket) socket = io(`${import.meta.env.VITE_URL_BACKEND}`);
   if (!socket) throw new Error('Falha ao criar a conexão com socket');
   return socket;
 };
--- src/utils/ProviderInitBot.tsx
+++ src/utils/ProviderInitBot.tsx
@@ -2,16 +2,16 @@ import { TypeConfig } from "../types/TypeConfig";
 import GetSocket from "./GetSocket";
 import ProviderNotification from "./ProviderNotification";
 
-//TODO - essa função deve emitir e ouvir evento de BOT não de CONFIG
 export default function ProviderInitBot(config: TypeConfig) {
   const socket = GetSocket();
-  const handleConfigPutRes = (response: { title: string, message: string }) => {
+
+  const handleEntryPostRes = (response: { title: string, message: string }) => {
     const { title, message } = response;
     ProviderNotification({ title, message });
 
-    socket.off('CONFIG_PUT_RES', handleConfigPutRes);
+    socket.off('ENTRY_POST_RES', handleEntryPostRes);
   };
-  socket.on('CONFIG_PUT_RES', handleConfigPutRes);
+  socket.on('ENTRY_POST_RES', handleEntryPostRes);
 
-  socket.emit('CONFIG_PUT', config._id, { CONFIG_STATUS: !config.CONFIG_STATUS });
+  socket.emit('ENTRY_POST', config);
 }
