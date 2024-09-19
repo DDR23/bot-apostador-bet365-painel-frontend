6ea112b0f2915866318bf38eaf82fa92548cc2d5
omegatech-andre <omega.ti_ind@outlook.com>
2024-09-19 17:32:55 -0300

    save

--- src/middleware/ProviderInitBot.tsx
+++ src/middleware/ProviderInitBot.tsx
@@ -5,27 +5,30 @@ import ProviderNotification from "../utils/ProviderNotification";
 export default function ProviderInitBot(config: TypeConfig) {
   const socket = GetSocket();
 
+  // Função para lidar com a resposta da configuração
   const handleConfigPutRes = (response: { title: string, message: string, data: TypeConfig }) => {
     const { title, message, data } = response;
-    console.log('bot iniciado', data)
-    if (title === 'Erro') ProviderNotification({ title, message })
-    ProviderNotification({ title, message: 'Bot foi iniciado!' });
+
+    if (title === 'Erro') {
+      ProviderNotification({ title, message });
+      return;
+    }
 
     if (title === 'Sucesso') {
       const handleEntryPostRes = (response: { title: string, message: string }) => {
         const { title, message } = response;
         ProviderNotification({ title, message });
-    
+
         socket.off('ENTRY_POST_RES', handleEntryPostRes);
       };
       socket.on('ENTRY_POST_RES', handleEntryPostRes);
-    
+
       socket.emit('ENTRY_POST', data);
     }
 
     socket.off('CONFIG_PUT_RES', handleConfigPutRes);
   };
-  socket.on('CONFIG_PUT_RES', handleConfigPutRes);
 
+  socket.on('CONFIG_PUT_RES', handleConfigPutRes);
   socket.emit('CONFIG_PUT', config._id, { CONFIG_STATUS: true });
 }
