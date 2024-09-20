8e5db52d11fc977b6eb1d884cca10c25da3d1741
omegatech-andre <omega.ti_ind@outlook.com>
2024-09-20 16:19:16 -0300

    feat:{adicionando ProviderStopBot}

--- src/middleware/ProviderInitBot.tsx
+++ src/middleware/ProviderInitBot.tsx
@@ -5,18 +5,13 @@ import ProviderNotification from "../utils/ProviderNotification";
 export default function ProviderInitBot(config: TypeConfig) {
   const socket = GetSocket();
 
-  // Função para lidar com a resposta da configuração
   const handleConfigPutRes = (response: { title: string, message: string, data: TypeConfig }) => {
     const { title, message, data } = response;
-    if (title === 'Erro') {
-      ProviderNotification({ title, message });
-      return;
-    }
-    if (title === 'Sucesso') {
-      socket.emit('SCRAPER_START', data);
-    }
+    if (title === 'Erro') ProviderNotification({ title, message });
+    if (title === 'Sucesso') socket.emit('SCRAPER_START', data);
     socket.off('CONFIG_PUT_RES', handleConfigPutRes);
   };
   socket.on('CONFIG_PUT_RES', handleConfigPutRes);
+
   socket.emit('CONFIG_PUT', config._id, { CONFIG_STATUS: true });
 }
--- src/middleware/ProviderStopBot.tsx
+++ src/middleware/ProviderStopBot.tsx
@@ -7,27 +7,27 @@ export default function ProviderStopBot(config: TypeConfig) {
 
   const handleConfigPutRes = (response: { title: string, message: string, data: TypeConfig }) => {
     const { title, message, data } = response;
-    
-    if (title === 'Erro') {
-      ProviderNotification({ title, message });
-      return;
-    }
-
-    // if (title === 'Sucesso') {
-    //   const handleEntryPostRes = (response: { title: string, message: string }) => {
-    //     const { title, message } = response;
-    //     ProviderNotification({ title, message });
-    
-    //     socket.off('ENTRY_POST_RES', handleEntryPostRes);
-    //   };
-    //   socket.on('ENTRY_POST_RES', handleEntryPostRes);
-    
-    //   socket.emit('ENTRY_POST', data);
-    // }
-
+    if (title === 'Erro') ProviderNotification({ title, message });
+    if (title === 'Sucesso') socket.emit('SCRAPER_STOP', data);
     socket.off('CONFIG_PUT_RES', handleConfigPutRes);
   };
   socket.on('CONFIG_PUT_RES', handleConfigPutRes);
 
   socket.emit('CONFIG_PUT', config._id, { CONFIG_STATUS: false });
 }
+
+
+
+
+
+
+
+// ESSA PARTE DE BAIXO DEVE SER OUVIDA NAS PAGES
+
+//   const handleEntryPostRes = (response: { title: string, message: string }) => {
+//     const { title, message } = response;
+//     ProviderNotification({ title, message });
+
+//     socket.off('ENTRY_POST_RES', handleEntryPostRes);
+//   };
+//   socket.on('ENTRY_POST_RES', handleEntryPostRes);
